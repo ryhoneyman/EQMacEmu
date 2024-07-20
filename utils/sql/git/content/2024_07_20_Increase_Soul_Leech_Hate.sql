62e42245a452714f4efa101f161efe1a3ac69ae8
ryhoneyman <ryhoneyman@gmail.com>
2024-07-20 22:59:52 +0000

    database content updates

---
+++ utils/sql/git/content/2024_07_20_Increase_Soul_Leech_Hate.sql
@@ -0,0 +1,2 @@
+/* Change chance of Cazic Thule in fear to drop SHD epic component 'Soul Leech' from 50% to 100% (item_id:4200012, loottable_id:2200034, lootdrop_id:11609) */
+update lootdrop_entries set chance = 100 where lootdrop_id = 2200034 and item_id = 11609;
