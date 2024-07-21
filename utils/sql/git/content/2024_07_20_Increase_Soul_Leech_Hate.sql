03521a09263d4cbc21121fe1d258b7a82823869d
ryhoneyman <ryhoneyman@gmail.com>
2024-07-21 10:14:55 +0000

    fixed probability, instead of chance

--- utils/sql/git/content/2024_07_20_Increase_Soul_Leech_Hate.sql
+++ utils/sql/git/content/2024_07_20_Increase_Soul_Leech_Hate.sql
@@ -1,2 +1,2 @@
-/* Change chance of Cazic Thule in fear to drop SHD epic component 'Soul Leech' from 50% to 100% (item_id:4200012, loottable_id:2200034, lootdrop_id:11609) */
-update lootdrop_entries set chance = 100 where lootdrop_id = 2200034 and item_id = 11609;
+/* Change probability of Cazic Thule in fear to drop SHD epic component 'Soul Leech' from 50% to 100% (loottable_id:4200012, lootdrop_id:2200034) */
+update loottable_entries set probability = 100 where loottable_id = 4200012 and lootdrop_id = 2200034;
